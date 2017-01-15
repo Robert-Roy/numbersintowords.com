fe8defe055d66ced099a67e1f278e1360c22df7b
Robert-Roy <robertproy@live.com>
2017-01-15 12:42:13 -0600

    git init

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!--
+All rights reserved. Copyright Robert Roy 2016.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>            
+        <label class="label" for="message">Enter a number:</label>
+        <p id="output"></p>
+    </body>
+</html>
